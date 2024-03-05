82a3106bfd433e4186175f38c1038add176e2e0a
LudCano <ludving.adolfo@gmail.com>
2024-03-05 13:13:28 -0400

    Titulo

--- merging.py
+++ merging.py
@@ -1,8 +1,8 @@
 ## CODIGO DE CORRELACION ENTRE TABLAS
 ## ---------------------------------------
 ## PARAMETROS EJECUCION
-date_start  = "2023-01-01"            #fecha de inicio
-date_fin    = "2023-12-31"            #fecha de fin
+date_start  = "2022-01-01"            #fecha de inicio
+date_fin    = "2022-12-31"            #fecha de fin
 manual_path = "data/manual.csv"       #ruta a archivo procesado pluvio manual
 auto_path   = "data/automatico.csv"   #ruta a archivo procesado pluvio automatico
 ## ---------------------------------------
@@ -73,26 +73,31 @@ manual["d_pluv"] = pluviomanual - manual.acum
 manual["d_pluv2"] = pluviomanual - manual.acum_m1
 
 # Cantidad de eventos registrados por observador
-bxdata = []; bxdata2 = []
+bxdata = []; bxdata2 = []; names_wcount = []
 for j in observers:
     aux = manual[manual.observador == j]
     print(j, len(aux))
     bxdata.append(aux.d_pluv)
     bxdata2.append(aux.d_pluv2)
+    names_wcount.append(f"{j}-{len(aux)}")
+
+
+
 
 fig, ax = plt.subplots(1,1)
 ax.boxplot(bxdata)
 ax.axhline(0, c = "k", lw = 0.5, alpha = .5)
 ax.set_xticklabels(observers)
 ax.set_ylabel("p_obs - p_auto")
-ax.set_title("Diferencia entre medidas")
+ax.set_title(f"Diferencia entre medidas\n{date_start} al {date_fin}")
 
 
 
 fig, ax2 = plt.subplots(1,1)
 ax2.boxplot(bxdata2)
 ax2.axhline(0, c = "k", lw = 0.5, alpha = .5)
-ax2.set_xticklabels(observers)
+ax2.set_xticklabels(names_wcount) #mostradno nombres con contador
+#ax2.set_xticklabels(observers)    #mostrando solo nombres
 ax2.set_ylabel("p_obs - p_auto")
-ax2.set_title("Diferencia entre medidas + 1 hora")
+ax2.set_title(f"Diferencia entre medidas + 1 hora\n{date_start} al {date_fin}")
 plt.show()
