f9a4f34c255722d26bd520c9a34e7722a5eb4bf7
LudCano <ludving.adolfo@gmail.com>
2024-03-05 13:26:44 -0400

    Conteo

--- merging.py
+++ merging.py
@@ -5,6 +5,7 @@ date_start  = "2022-01-01"            #fecha de inicio
 date_fin    = "2022-12-31"            #fecha de fin
 manual_path = "data/manual.csv"       #ruta a archivo procesado pluvio manual
 auto_path   = "data/automatico.csv"   #ruta a archivo procesado pluvio automatico
+threshold   = 10                       #cantidad de eventos para tomar en cuenta un observador
 ## ---------------------------------------
 
 # Librerias
@@ -73,13 +74,15 @@ manual["d_pluv"] = pluviomanual - manual.acum
 manual["d_pluv2"] = pluviomanual - manual.acum_m1
 
 # Cantidad de eventos registrados por observador
-bxdata = []; bxdata2 = []; names_wcount = []
+bxdata = []; bxdata2 = []; names_wcount = []; obs_names = []
 for j in observers:
     aux = manual[manual.observador == j]
     print(j, len(aux))
-    bxdata.append(aux.d_pluv)
-    bxdata2.append(aux.d_pluv2)
-    names_wcount.append(f"{j}-{len(aux)}")
+    if len(aux) >= threshold:
+        bxdata.append(aux.d_pluv)
+        bxdata2.append(aux.d_pluv2)
+        names_wcount.append(f"{j}-{len(aux)}")
+        obs_names.append(j)
 
 
 
@@ -87,7 +90,8 @@ for j in observers:
 fig, ax = plt.subplots(1,1)
 ax.boxplot(bxdata)
 ax.axhline(0, c = "k", lw = 0.5, alpha = .5)
-ax.set_xticklabels(observers)
+#ax.set_xticklabels(obs_names) #nombres y conteo
+ax.set_xticklabels(names_wcount) #solo nombres
 ax.set_ylabel("p_obs - p_auto")
 ax.set_title(f"Diferencia entre medidas\n{date_start} al {date_fin}")
 
@@ -97,7 +101,7 @@ fig, ax2 = plt.subplots(1,1)
 ax2.boxplot(bxdata2)
 ax2.axhline(0, c = "k", lw = 0.5, alpha = .5)
 ax2.set_xticklabels(names_wcount) #mostradno nombres con contador
-#ax2.set_xticklabels(observers)    #mostrando solo nombres
+#ax2.set_xticklabels(obs_names)    #mostrando solo nombres
 ax2.set_ylabel("p_obs - p_auto")
 ax2.set_title(f"Diferencia entre medidas + 1 hora\n{date_start} al {date_fin}")
 plt.show()
